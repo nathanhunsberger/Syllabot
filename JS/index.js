cdec5672b6f8e9b9dc454a79766cb329c09efab7
nathanhunsberger <nathan.t.hunsberger@vanderbilt.edu>
2022-11-19 11:59:44 -0800

    Clean up index.js

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,8 @@
-console.log("H")
-
 function onClick() {
 
   const fileField = document.querySelector('input[type="file"]');
-  //const formData     = new FormData();
+
+  //url_func is a test AWS function I have to quickly test stuff, the one we use to call the model is the url_image
   const url_func = "https://1s4pf1fpk3.execute-api.us-east-2.amazonaws.com/test/syllabi-test";
   const url_image = "https://1s4pf1fpk3.execute-api.us-east-2.amazonaws.com/beta/nocors";
   getBase64(fileField.files[0]).then(function(data) {
@@ -11,9 +10,9 @@ function onClick() {
     const bod = {
       "file": data.substring(28)
     }
-    console.log(data)
     fetch(url_image, {
       method: 'POST',
+      // Weird CORS errors, trying to find fix. This stuff doesn't quite do it though
       headers: {
       "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
       "Access-Control-Allow-Origin": "*",
@@ -26,32 +25,9 @@ function onClick() {
     .then(response => console.log(response))
   });
   console.log(fileField.files[0])
-
-  
-  
-
-
-  // fetch(url_func, {
-  //   method: 'POST',
-  //   //mode: "no-cors",
-  //   // headers: {
-  //   //   'content-type': 'application/pdf',
-  //   //   // "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
-  //   //   // "Access-Control-Allow-Origin": "*",
-  //   //   // "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
-  //   //   // "X-Requested-With": "*"
-  //   // },
-  //   body: formData
-  // })
-  //   .then((response) => response.json())
-  //   .then((result) => {
-  //     console.log('Success:', result);
-  //   })
-  //   .catch((error) => {
-  //     console.error('Error:', error);
-  //   });
 }
 
+// I convert the PDF to a base64 string so we can POST it to the api more easily
 function getBase64(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -60,5 +36,3 @@ function getBase64(file) {
     reader.onerror = error => reject(error);
   });
 }
-
-//var file = document.querySelector('input[type="file"]').files[0];
