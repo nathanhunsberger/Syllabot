9e09a8675d5c34f964d5441a9a65e7da720f71c1
b-walton <benjamin.t.walton@vanderbilt.edu>
2023-01-12 16:16:12 -0600

    updated load screen

--- src/frontend/src/Pages/Loading.js
+++ src/frontend/src/Pages/Loading.js
@@ -1,6 +1,13 @@
-function Loading(){
 
+import './Loading.css'
+import circle_logo from '../Images/circle_logo.png'
+import logo from '../Images/basic_logo.png'
+function Loading(){
     return (
-        <h1>Loading Assignments</h1>
+        <div>
+            <img className= 'logo' src = {logo} alt = "Syllabot Logo"/>
+            <br></br>
+        <img src = {circle_logo} alt = 'logo' className='Circle-logo' />
+        </div>
     )
 }export default Loading;
